cc31beab41a58fc8e62b79d8cd2774e22b8b80d8
rohanpillai999 <70111433+rohanpillai999@users.noreply.github.com>
2026-03-03 19:35:28 +0000

    game states and basic game loop

--- src/game.py
+++ src/game.py
@@ -1,4 +1,5 @@
 import pygame
+from enum import Enum
 
 from pygame.locals import (
     K_UP,
@@ -13,11 +14,21 @@ from pygame.locals import (
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 800
 
+class GameState(Enum):
+    PLAYING = 1
+    GAME_OVER = 2
+    MENU = 3
+
 pygame.init()
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-running = True
+state = GameState.PLAYING
 
-while running:
-    screen.fill((135, 206, 250))
+while True:
+    if state == GameState.PLAYING:
+        screen.fill((135, 206, 250))
+    elif state == GameState.GAME_OVER:
+        screen.fill((0, 0, 0))
+    elif state == GameState.MENU:
+        screen.fill((0, 0, 0))
