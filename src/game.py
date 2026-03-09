e13de77d6c7b97bfe8868da9076a0bcbbff85c14
rohanpillai999 <70111433+rohanpillai999@users.noreply.github.com>
2026-03-09 18:34:47 +0000

    smoother motion (still needs debugging)

--- src/game.py
+++ src/game.py
@@ -20,16 +20,15 @@ class Player(pygame.sprite.Sprite):
         self.surf = pygame.image.load("src/assets/flappy.png").convert()
         self.rect = self.surf.get_rect()
         self.last_jump = 0
-    def update(self, pressed_keys):
-        if pressed_keys[K_UP]:
+        self.speed = 0
+    def update(self, jumped):
+        if jumped:
             if pygame.time.get_ticks() - self.last_jump >= 90:
-                self.rect.move_ip(0, -30)
+                self.speed -= 30
                 self.last_jump = pygame.time.get_ticks()
-        self.rect.move_ip(0, 5)
-        if pressed_keys[K_LEFT]:
-            self.rect.move_ip(-5, 0)
-        if pressed_keys[K_RIGHT]:
-            self.rect.move_ip(5, 0)
+        
+        self.speed += 5
+        self.rect.move_ip(0, self.speed)
         
         # Keep player on the screen
         if self.rect.left < 0:
@@ -40,58 +39,17 @@ class Player(pygame.sprite.Sprite):
             self.rect.top = 0
         if self.rect.bottom >= SCREEN_HEIGHT:
             self.rect.bottom = SCREEN_HEIGHT
+            return True
+        return False
+    def reset(self):
+        self.rect.left = SCREEN_WIDTH // 2
+        self.rect.top = SCREEN_HEIGHT // 2
 
-class Enemy(pygame.sprite.Sprite):
-    def __init__(self):
-        super(Enemy, self).__init__()
-        self.surf = pygame.image.load("missile.png").convert()
-        self.surf.set_colorkey((255, 255, 255), RLEACCEL)
-        self.rect = self.surf.get_rect(
-            center=(
-                random.randint(SCREEN_WIDTH + 20, SCREEN_WIDTH + 100),
-                random.randint(0, SCREEN_HEIGHT),
-            )
-        )
-        self.speed = random.randint(5, 20)
 
-    # Move the sprite based on speed
-    # Remove the sprite when it passes the left edge of the screen
-    def update(self):
-        self.rect.move_ip(-self.speed, 0)
-        if self.rect.right < 0:
-            self.kill()
-
-# Define the cloud object by extending pygame.sprite.Sprite
-# Use an image for a better-looking sprite
-class Cloud(pygame.sprite.Sprite):
-    def __init__(self):
-        super(Cloud, self).__init__()
-        self.surf = pygame.image.load("cloud.png").convert()
-        self.surf.set_colorkey((0, 0, 0), RLEACCEL)
-        # The starting position is randomly generated
-        self.rect = self.surf.get_rect(
-            center=(
-                random.randint(SCREEN_WIDTH + 20, SCREEN_WIDTH + 100),
-                random.randint(0, SCREEN_HEIGHT),
-            )
-        )
-
-    # Move the cloud based on a constant speed
-    # Remove the cloud when it passes the left edge of the screen
-    def update(self):
-        self.rect.move_ip(-5, 0)
-        if self.rect.right < 0:
-            self.kill()
 
 # Create the screen object
 # The size is determined by the constant SCREEN_WIDTH and SCREEN_HEIGHT
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
-# Create a custom event for adding a new enemy
-ADDENEMY = pygame.USEREVENT + 1
-pygame.time.set_timer(ADDENEMY, 250)
-ADDCLOUD = pygame.USEREVENT + 2
-pygame.time.set_timer(ADDCLOUD, 1000)
 
 # Create the 'player'
 player = Player()
@@ -99,8 +57,7 @@ player = Player()
 # Create groups to hold enemy sprites and all sprites
 # - enemies is used for collision detection and position updates
 # - all_sprites is used for rendering
-enemies = pygame.sprite.Group()
-clouds = pygame.sprite.Group()
+
 all_sprites = pygame.sprite.Group()
 all_sprites.add(player)
 
@@ -111,29 +68,35 @@ clock = pygame.time.Clock()
 state = GameState.MENU
 
 while running:
-    for event in pygame.event.get():
-        if event.type == KEYDOWN:
-            if event.key == K_ESCAPE:
-                state = GameState.MENU
-            elif event.key == K_p:
-                state = GameState.PLAYING
-        
+    # events = pygame.event.get()
+    pressed_keys = pygame.key.get_pressed()
+    
+    
 
-    if state == GameState.PLAYING:
-        pressed_keys = pygame.key.get_pressed()
-        player.update(pressed_keys)
+    if state == GameState.MENU:
+
+        if pressed_keys[K_p]:
+            state = GameState.PLAYING
+            player.reset()
+        screen.fill((0, 0, 0))
+    elif state == GameState.GAME_OVER:
+        if pressed_keys[K_ESCAPE]:
+            state = GameState.MENU
+        screen.fill((255, 0, 0))
+    elif state == GameState.PLAYING:
+        dead = False
+
+        if pressed_keys[K_UP]:
+            dead = player.update(True)
+        else:
+            dead = player.update(False)
+
         screen.fill((135, 206, 250))
         for entity in all_sprites:
             screen.blit(entity.surf, entity.rect)
         
-        if player.rect.bottom >= SCREEN_HEIGHT:
+        if dead:
             state = GameState.GAME_OVER
-            player.rect.left = 0
-            player.rect.top = 0
-    elif state == GameState.GAME_OVER:
-        screen.fill((255, 0, 0))
-    elif state == GameState.MENU:
-        screen.fill((0, 0, 0))
     
     pygame.display.flip()
     
